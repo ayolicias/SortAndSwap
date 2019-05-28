f4b02008a8e36f990670b781b343433ed26f0ae1
ayolicias <ziyandandlamafa47@gmail.com>
2019-05-28 16:21:30 +0200

    first commit

--- src/main/java/co/projectcodex/sortandswap/SwapMe.java
+++ src/main/java/co/projectcodex/sortandswap/SwapMe.java
@@ -3,19 +3,38 @@ package co.projectcodex.sortandswap;
 public class SwapMe {
 
     public static String[] swapFirstWithLast(String[] values) {
-        return null;
+        String firstName = values[0];
+        values[0] = values[values.length -1];
+        values[values.length -1] = firstName;
+        return values;
     }
 
     public static String[] swapFirstWithSecond(String[] values) {
-        return null;
+        String firstName = values[0];
+
+        values[0] = values[1];
+        values[1] = firstName;
+
+        return values;
     }
 
     public static int[] swapNeighbours(int[] numbers) {
-        return null;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (i + 1 < numbers.length) {
+                int current = numbers[i];
+                int next = numbers[i + 1];
+
+                if (current > next) {
+                    numbers[i] = next;
+                    numbers[i+1] = current;
+
+                }
+            }
+
+        }
+        return numbers;
+
     }
-
-
-
-
-
-}
+    }
